3f4aa83a49ec4f8f179d8b9ee42392c1bc4ddeb3
loctran <tranloc1906@gmail.com>
2023-06-03 01:15:11 +0700

    header

--- src/pages/dashboard/Header.tsx
+++ src/pages/dashboard/Header.tsx
@@ -2,8 +2,13 @@
 import Image from 'next/image';
 import React from 'react';
 
+import bg from '../../../public/assets/images/header/btn.png';
 import btnLeft from '../../../public/assets/images/header/btnleft.png';
+import cart from '../../../public/assets/images/header/cart.png';
+import exit from '../../../public/assets/images/header/exit.png';
+import game from '../../../public/assets/images/header/game.png';
 import iconLeft from '../../../public/assets/images/header/lefticon.png';
+import list from '../../../public/assets/images/header/list.png';
 import styles from './dashboard.module.scss';
 
 const Header = () => {
@@ -40,7 +45,56 @@ const Header = () => {
           </div>
         </button>
       </div>
-      <div>ok1</div>
+      <div className="d-flex justify-content-end align-items-center gap-4">
+        <button
+          type="button"
+          className="btn btn-link position-relative d-flex align-items-center m-0 p-0"
+        >
+          <Image itemType="button" src={bg} alt="Score" />
+          <div
+            className="d-flex justify-content-center align-items-center position-absolute rounded-circle inset-x-0 m-auto bg-white"
+            style={{ width: '75%', height: '75%' }}
+          >
+            <Image itemType="button" src={game} alt="Score" />
+          </div>
+        </button>
+        <button
+          type="button"
+          className="btn btn-link position-relative d-flex align-items-center m-0 p-0"
+        >
+          <Image itemType="button" src={bg} alt="Score" />
+          <div
+            className="d-flex justify-content-center align-items-center position-absolute rounded-circle inset-x-0 m-auto bg-white"
+            style={{ width: '75%', height: '75%' }}
+          >
+            <Image itemType="button" src={list} alt="Score" />
+          </div>
+        </button>
+        <button
+          type="button"
+          className="btn btn-link position-relative d-flex align-items-center m-0 p-0"
+        >
+          <Image itemType="button" src={bg} alt="Score" />
+          <div
+            className="d-flex justify-content-center align-items-center position-absolute rounded-circle inset-x-0 m-auto bg-white"
+            style={{ width: '75%', height: '75%' }}
+          >
+            <Image itemType="button" src={cart} alt="Score" />
+          </div>
+        </button>
+        <button
+          type="button"
+          className="btn btn-link position-relative d-flex align-items-center m-0 p-0"
+        >
+          <Image itemType="button" src={bg} alt="Score" />
+          <div
+            className="d-flex justify-content-center align-items-center position-absolute rounded-circle inset-x-0 m-auto bg-white"
+            style={{ width: '75%', height: '75%' }}
+          >
+            <Image itemType="button" src={exit} alt="Score" />
+          </div>
+        </button>
+      </div>
     </div>
   );
 };
